45281b11c8d5ef43a2ecf57590b3ecef4b3437e8
Avlasova Kateryna <demix11jr@gmail.com>
2025-05-23 15:16:28 +0300

    chore(dal):remove address property from UserDto model

--- BLL/dto/UserDto.cs
+++ BLL/dto/UserDto.cs
@@ -9,7 +9,6 @@ namespace BLL.dto
         public string? Username { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
-        public string? Address { get; set; }
         public Roles Role { get; set; }
         public OrderDto Order { get; set; }
     }
