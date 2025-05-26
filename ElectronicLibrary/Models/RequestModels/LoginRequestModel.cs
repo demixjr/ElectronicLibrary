2ab66d8ffb95612a963b6b38ec83ef370d86c23c
Yulia Benediuk <8475892@stud.nau.edu.ua>
2025-05-26 12:30:30 +0300

    feat(pl): add validation attributes for LoginRequestModel, UserRequestModel and PrivilegedUserRequestModel

--- ElectronicLibrary/Models/RequestModels/LoginRequestModel.cs
+++ ElectronicLibrary/Models/RequestModels/LoginRequestModel.cs
@@ -1,8 +1,16 @@
-﻿namespace ElectronicLibrary.Models.RequestModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ElectronicLibrary.Models.RequestModels
 {
     public class LoginRequestModel
     {
+        [Required(ErrorMessage = "Email є обов’язковим")]
+        [EmailAddress(ErrorMessage = "Некоректний формат email")]
         public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Пароль є обов’язковим")]
+        [MinLength(6, ErrorMessage = "Пароль має містити щонайменше 6 символів")]
+        [MaxLength(64, ErrorMessage = "Пароль не може перевищувати 64 символи")]
         public string? Password { get; set; }
     }
 }
--- ElectronicLibrary/Models/RequestModels/UserRequestModel.cs
+++ ElectronicLibrary/Models/RequestModels/UserRequestModel.cs
@@ -1,10 +1,22 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace ElectronicLibrary.Models.RequestModels
 {
     public class UserRequestModel
     {
+        [Required(ErrorMessage = "Ім’я користувача є обов’язковим")]
+        [MinLength(3, ErrorMessage = "Ім’я користувача має містити щонайменше 3 символи")]
+        [MaxLength(32, ErrorMessage = "Ім’я користувача не може перевищувати 32 символи")]
         public string? Username { get; set; }
+
+        [Required(ErrorMessage = "Email є обов’язковим")]
+        [EmailAddress(ErrorMessage = "Некоректний формат email")]
         public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Пароль є обов’язковим")]
+        [MinLength(6, ErrorMessage = "Пароль має містити щонайменше 6 символів")]
+        [MaxLength(64, ErrorMessage = "Пароль не може перевищувати 64 символи")]
         public string? Password { get; set; }
     }
 }
