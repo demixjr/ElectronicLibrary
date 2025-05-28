c2b394aac599d211e75d48cc8856ee2b053b7482
Yulia Benediuk <8475892@stud.nau.edu.ua>
2025-05-28 15:54:55 +0300

    feat(pl): add helper static class ClaimsPrincipalExtensions

---
+++ ElectronicLibrary/Helpers/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+
+namespace ElectronicLibrary.Helpers
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static bool TryGetUserId(this ClaimsPrincipal user, out int userId)
+        {
+            userId = 0;
+            var userIdClaim = user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            return userIdClaim != null && int.TryParse(userIdClaim, out userId);
+        }
+    }
+}
